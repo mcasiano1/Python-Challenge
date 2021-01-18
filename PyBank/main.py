4002c2874f37ba2562a92aaabb37b7096fd3ab74
mcassiano1 <yssa_casiano@yahoo.com>
2021-01-17 19:56:28 -0600

    push

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,8 @@ import os
 import csv
 months=[]
 pl=[]
-Totalpl=[]
+AvgChange=[]
+mchange=[]
 NetTotal=0
 
 # Set path for file
@@ -14,12 +15,29 @@ with open(csvpath,newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #seperate header
     header = next(csvreader)
-    # Loop through data
+    # Loop through data and calc number of months in ds
     for row in csvreader:
         months.append(row[0])
-        pl.append(row[1])       
+        pl.append(row[1])  
+        TotalMonth=len(months)
+        #Calc net total amt of P/L     
         NetTotal=NetTotal + int (row[1]) 
 
-TotalMonth=len(months)
 print(f"Total Months: {TotalMonth}")
 print(f"Total: ${NetTotal}")
+
+#Calc Average Change
+i=0
+for i in range(len(pl)-1):
+    p_loss= int(pl[i+1])-int(pl[i])
+#Append p_loss
+    AvgChange.append(p_loss)
+Total=sum(AvgChange)
+mchange=Total/len(AvgChange)
+m_format=mchange
+Formatted_m="{:.2f}".format(m_format)
+print(f"Average Change: ${Formatted_m}")
+
+
+
+
